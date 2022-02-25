30bced1f502fe5b8ed3622ed48008e371390174b
Gabriel Augusto <gabrielolivrp@gmail.com>
2022-02-25 18:08:51 -0300

    fix dropdown items

--- packages/web/src/components/Dropdown/index.tsx
+++ packages/web/src/components/Dropdown/index.tsx
@@ -12,7 +12,7 @@ export interface DropdownItemProps {
 export const DropdownItem = ({ text, onClick, icon: Icon }: DropdownItemProps) => (
   <Menu.Item>
     {({ active }) => (
-      <button onClick={onClick} className={`${active && 'text-primary dark:text-primary'} group flex rounded-md items-center space-x-2 w-full px-2 py-2 text-sm text-gray-700 dark:text-gray-50`}>
+      <button onClick={onClick} className={`group flex rounded-md items-center space-x-2 w-full px-2 py-2 text-md ${active ? 'text-primary dark:text-primary' : 'text-gray-700 dark:text-gray-50'}`}>
         {Icon && <Icon />}
         <label className="cursor-pointer">{text}</label>
       </button>
@@ -30,7 +30,6 @@ const Dropdown = ({ button, children }: DropdownProps) => {
     <Menu as="div" className="relative inline-block text-left">
       <Menu.Button as="div">{button}</Menu.Button>
       <Transition
-        as={Fragment}
         enter="transition ease-out duration-100"
         enterFrom="transform opacity-0 scale-95"
         enterTo="transform opacity-100 scale-100"
