c2559929c628dde213b3c174a169f688f84cae17
Gabriel Augusto <gabrielolivrp@gmail.com>
2022-02-26 15:14:48 -0300

    remove `className' prop

--- packages/web/src/components/Card/index.tsx
+++ packages/web/src/components/Card/index.tsx
@@ -2,12 +2,11 @@ interface CardProps {
   children: React.ReactNode
   title?: string
   action?: React.ReactNode
-  className?: string
 }
 
-const Card = ({ children, className, title, action }: CardProps) => (
-  <div className={`${className} bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-50 rounded-3xl shadow-lg`}>
-    <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+const Card = ({ children, title, action }: CardProps) => (
+  <div className="w-full h-full bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-50 rounded-3xl shadow-lg">
+    < div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4" >
       <div className="text-left w-full">
         {!!title && (
           <div className="flex items-center justify-between leading-6">
