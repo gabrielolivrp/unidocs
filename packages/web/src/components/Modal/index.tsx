bea63897120851cf52a513f039185a7e214f2a93
Gabriel Augusto <gabrielolivrp@gmail.com>
2022-02-26 15:16:09 -0300

    fix modal resize

--- packages/web/src/components/Modal/index.tsx
+++ packages/web/src/components/Modal/index.tsx
@@ -24,8 +24,7 @@ const Modal = ({ children, title, open = false, onClose }: ModalProps) => {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={onClose}>
-        <div
-          className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center min-h-screen px-8 text-center">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -37,7 +36,7 @@ const Modal = ({ children, title, open = false, onClose }: ModalProps) => {
           >
             <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-40 dark:bg-gray-800 dark:bg-opacity-40 transition-opacity" />
           </Transition.Child>
-          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+          <span className="hidden" aria-hidden="true">
             &#8203;
           </span>
           <Transition.Child
@@ -49,7 +48,7 @@ const Modal = ({ children, title, open = false, onClose }: ModalProps) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className='inline-block align-bottom overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:max-w-md sm:w-full'>
+            <div className="overflow-hidden transform transition-all w-full sm:max-w-md">
               <Card
                 title={title}
                 action={<CloseModalButton onClick={onClose} />}
