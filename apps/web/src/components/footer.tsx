2e453bf7b4c0611cb65670f52697b1a5667a2fa4
Gabriel Augusto <gabrielolivrp@gmail.com>
2024-04-23 17:40:27 -0300

    fix(footer): github link

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -1,18 +1,19 @@
 import { Button, Icon, Typography } from "@unidocs/ui";
 
 const Footer = () => (
-  <footer className="container mb-10">
+  <footer className="container mb-10 border-t">
     <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
       <div className="space-y-2">
         <Typography variant="h5">
-          &copy; {new Date().getFullYear()}{" "}
-          <a href="https://github.com/gabrielolivrp/unidocs">Unidocs</a>
+          &copy; {new Date().getFullYear()} unidocs
         </Typography>
 
         <div className="space-x-2">
-          <Button variant="ghost">
-            <Icon name="Github" />
-          </Button>
+          <a href="https://github.com/gabrielolivrp/unidocs" target="_blank">
+            <Button variant="ghost">
+              <Icon name="Github" />
+            </Button>
+          </a>
           <Button variant="ghost">
             <Icon name="Instagram" />
           </Button>
