03ea7d806a037dbd2ca219e903d7dc2a68aef08f
Gabriel Augusto <gabrielolivrp@gmail.com>
2024-07-12 09:42:39 -0300

    feat(contracts): add sepolia network

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -1,5 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
+
+if (!ALCHEMY_API_KEY) {
+  throw new Error("ALCHEMY_API_KEY");
+}
+
+if (!DEPLOYER_PRIVATE_KEY) {
+  throw new Error("DEPLOYER_PRIVATE_KEY");
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -9,6 +23,12 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [DEPLOYER_PRIVATE_KEY],
     },
   },
 };
