5dc430137115c6ef6b32c5512869381607e11ee5
Gabriel Augusto <gabrielolivrp@gmail.com>
2024-11-24 20:00:50 -0300

    fix(contracts): tests

--- apps/contracts/hardhat.config.ts
+++ apps/contracts/hardhat.config.ts
@@ -7,6 +7,11 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
 const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
 
+const sepolia = {
+  url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+  accounts: [DEPLOYER_PRIVATE_KEY],
+};
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.24",
@@ -17,12 +22,7 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [DEPLOYER_PRIVATE_KEY],
-    },
-  },
+  networks: ALCHEMY_API_KEY && DEPLOYER_PRIVATE_KEY ? { sepolia } : {},
 };
 
 export default config;
